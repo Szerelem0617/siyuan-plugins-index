0f7874926dd8b130ffc8d4311c58c8f38747a9c4
Szerelem0617 <antimony00001@gmail.com>
2026-08-24 18:14:30 +0800

    chore: remove deprecated command/supertag directory

--- src/features/command/supertag/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * src/features/command/supertag/index.ts
- *
- * @deprecated Supertag and Unified Attributes have been refactored into `src/features/unified-attributes`.
- * Re-exporting from unified-attributes for transition safety.
- */
-
-export * from "../../unified-attributes";
